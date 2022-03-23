33b282b4fd2b68d74882ee1adbfb56b6f01e3d11
cooperrunyan <cooperrunyanspam@gmail.com>
2022-03-23 08:15:10 -0700

    added Use command

--- src/commands/group/Group.ts
+++ src/commands/group/Group.ts
@@ -3,11 +3,12 @@ import { Add } from './children/Add.js';
 import { List } from './children/List.js';
 import { Remove } from './children/Remove.js';
 import { Rename } from './children/Rename.js';
+import { Use } from './children/Use.js';
 
 export const Group = new Command({
   name: 'group',
   description: 'Control API request groups',
   aliases: ['g', 'groups'],
-  children: [Add, Rename, Remove, List],
+  children: [Add, Rename, Remove, List, Use],
   action() {},
 });
---
+++ src/commands/group/children/Use.ts
@@ -0,0 +1,8 @@
+import { Command } from '../../../models/Command.js';
+
+export const Use = new Command({
+  name: 'use',
+  aliases: ['u'],
+  description: 'Start using a certain group',
+  action() {},
+});
