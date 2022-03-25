8c650487218d6c3c967432abb47ff46e40b9b4ff
cooperrunyan <cooperrunyanspam@gmail.com>
2022-03-24 19:12:30 -0700

    Revert "Cat command"
    This reverts commit f09963492dbc0c8fef0eab541d9e27e2b1b1e827.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 
 import { Program } from './models/Program.js';
-import { getPackage } from './utils/getProjectRoot.js';
 
 import { Collection } from './commands/collection/Collection.js';
 import { GetStarted } from './commands/get-started/GetStarted.js';
 import { Request } from './commands/requests/Request.js';
 import { Send } from './commands/send/Send.js';
 import { Host } from './commands/host/Host.js';
-import { Cat } from './commands/cat/Cat.js';
+import { getPackage } from './utils/getProjectRoot.js';
 
 new Program({
   root: true,
   name: 'rest',
   version: getPackage().version,
   description: 'A CLI-Based REST client',
-  children: [Collection, Request, GetStarted, Send, Host, Cat],
+  children: [Collection, Request, GetStarted, Send, Host],
 });
